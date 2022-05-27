98dd7edd3c38b80aa0d8b98fd342bf9a3483dc7b
yufei3 <yzhao87@u.rochester.edu>
2022-05-27 11:18:13 -0400

    temp news page

--- src/pages/News.js
+++ src/pages/News.js
@@ -20,16 +20,39 @@ class News extends React.Component{
 
             <h3>News</h3>
             <div class="news-container">
-                <div class="slider">
-                    <div class="box1 box">
-                        <div class="bg">
-                            <div class="details">
-                               I'm here. 
-                            </div>
-                        </div>
+                <year>2022</year>
+                <month-slider>
+                    <div class="mysliders fade">
+                        <div class="number-text">1/12</div>
+                        <div class="slider-text">
+                            First Month
+                        </div> 
                     </div>
-                    
-                </div>
+
+                    <div class="mysliders fade">
+                        <div class="number-text">2/12</div>
+                        <div class="slider-text">
+                            Second Month
+                        </div> 
+                    </div>
+
+                    <div class="mysliders fade">
+                        <div class="number-text">3/12</div>
+                        <div class="slider-text">
+                            Third Month
+                        </div> 
+                    </div>
+
+                    /*Next and Previous buttons */
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </month-slider>
+
+                <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+</div>
             </div>
         </main>);
     }
