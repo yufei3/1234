0c5b506078c1e19676b3f4e883043122736817ed
yufei3 <yzhao87@u.rochester.edu>
2022-05-30 10:45:49 -0400

    news structure 1

--- src/pages/News.js
+++ src/pages/News.js
@@ -9,6 +9,38 @@ const CANONICAL = Config.SITE_DOMAIN + "/news";
 class News extends React.Component{
     
     render() {
+        
+        // let slides = document.getElementsByClassName("slider-slide");
+        // let navlinks = document.getElementsByClassName("slider-navlink");
+        // let currentSlide = 0;
+
+        // document.getElementById("nav-button-next").addEventListener("click", () => {
+        //     changeSlide(currentSlide + 1)
+        // });
+        // document.getElementById("nav-button-prev").addEventListener("click", () => {
+        //     changeSlide(currentSlide - 1)
+        // });
+
+        // function changeSlide(moveTo) {
+        //     if (moveTo >= slides.length) {moveTo = 0;}
+        //     if (moveTo < 0) {moveTo = slides.length - 1;}
+            
+        //     slides[currentSlide].classList.toggle("active");
+        //     navlinks[currentSlide].classList.toggle("active");
+        //     slides[moveTo].classList.toggle("active");
+        //     navlinks[moveTo].classList.toggle("active");
+            
+        //     currentSlide = moveTo;
+        // }
+
+        // document.querySelectorAll('.slider-navlink').forEach((bullet, bulletIndex) => {
+        //     bullet.addEventListener('click', () => {
+        //         if (currentSlide !== bulletIndex) {
+        //             changeSlide(bulletIndex);
+        //         }
+        //     })
+        // })
+
         return (<main>
             
              <Helmet>
@@ -22,37 +54,42 @@ class News extends React.Component{
             <div class="news-container">
                 <year>2022</year>
                 <month-slider>
-                    <div class="mysliders fade">
-                        <div class="number-text">1/12</div>
-                        <div class="slider-text">
-                            First Month
-                        </div> 
-                    </div>
+                <div class="slider-container">
+                    <div class="slider">
+                        <div class="slides">
+                        <div id="slides__1" class="slide">
+                            <span class="slide__text">1</span>
+                            <a class="slide__prev" href="#slides__4" title="Next"></a>
+                            <a class="slide__next" href="#slides__2" title="Next"></a>
+                        </div>
+                        <div id="slides__2" class="slide">
+                            <span class="slide__text">2</span>
+                            <a class="slide__prev" href="#slides__1" title="Prev"></a>
+                            <a class="slide__next" href="#slides__3" title="Next"></a>
+                        </div>
+                        <div id="slides__3" class="slide">
+                            <span class="slide__text">3</span>
+                            <a class="slide__prev" href="#slides__2" title="Prev"></a>
+                            <a class="slide__next" href="#slides__4" title="Next"></a>
+                        </div>
+                        <div id="slides__4" class="slide">
+                            <span class="slide__text">4</span>
+                            <a class="slide__prev" href="#slides__3" title="Prev"></a>
+                            <a class="slide__next" href="#slides__1" title="Prev"></a>
+                        </div>
+                        </div>
 
-                    <div class="mysliders fade">
-                        <div class="number-text">2/12</div>
-                        <div class="slider-text">
-                            Second Month
-                        </div> 
+                        <div class="slider__nav">
+                        <a class="slider__navlink" href="#slides__1"></a>
+                        <a class="slider__navlink" href="#slides__2"></a>
+                        <a class="slider__navlink" href="#slides__3"></a>
+                        <a class="slider__navlink" href="#slides__4"></a>
+                        </div>
                     </div>
-
-                    <div class="mysliders fade">
-                        <div class="number-text">3/12</div>
-                        <div class="slider-text">
-                            Third Month
-                        </div> 
-                    </div>
-
-                    /*Next and Previous buttons */
-                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
                 </month-slider>
 
-                <div style="text-align:center">
-                <span class="dot" onclick="currentSlide(1)"></span>
-                <span class="dot" onclick="currentSlide(2)"></span>
-                <span class="dot" onclick="currentSlide(3)"></span>
-</div>
+               
             </div>
         </main>);
     }
