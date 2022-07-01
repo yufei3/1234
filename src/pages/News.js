b0f0ec9fa951c5b82372c4b17bf8117f1589aa6b
yufei3 <yzhao87@u.rochester.edu>
2022-07-01 14:54:04 -0400

    calendar midway

--- src/pages/News.js
+++ src/pages/News.js
@@ -52,7 +52,24 @@ class News extends React.Component{
 
             <h3>News</h3>
             <div class="news-container">
-                <year>2022</year>
+                <year>2022
+                    <div class="container">
+                        <div class="grid">
+                            <div class="cell" id="1">1</div>
+                            <div class="cell" id="2">2</div>
+                            <div class="cell" id="3">3</div>
+                            <div class="cell" id="4">4</div>
+                            <div class="cell" id="5">5</div>
+                            <div class="cell" id="6">6</div>
+                            <div class="cell" id="7">7</div>
+                            <div class="cell" id="8">8</div>
+                            <div class="cell" id="9">9</div>
+                            <div class="cell" id="9">10</div>
+                            <div class="cell" id="9">11</div>
+                            <div class="cell" id="9">12</div>
+                        </div>              
+                    </div>
+                </year>
                 <month-slider>
                 <div class="slider-container">
                     <div class="slider">
